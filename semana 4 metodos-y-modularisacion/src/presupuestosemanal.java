eb249a84aafee56d00c7225e55e461ece215a7c2
Mynor-Giron <mynoradolfogironmuralles@gmail.com>
2026-08-02 13:09:44 -0600

    ejercicio semana 4

---
+++ semana 4 metodos-y-modularisacion/src/presupuestosemanal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class presupuestosemanal import java.util.Scanner;
+
+public class PresupuestoSemanal{
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ingrese su nombre: ");
+    String nombre = Scanner.nextline();
+
+        System.out.print("ingrese su presupuestosemanal: Q");
+    double presupuesto = scanner.nextDouble();
+
+        System.out.print("Gastos de alimentos:Q");
+    double alimentacion = scanner. nextDouble();
+
+        System.out.print("Gastos de transporte: Q");
+    double transporte = scanner.nextDouble();
+
+        System.out.print("Otros gastos: Q");
+    double otros = scanner. nextDouble();
+
+    escaner.clous
+}
